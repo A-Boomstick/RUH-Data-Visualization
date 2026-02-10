73e183200e5d4e158bbd473f06e71218953b43c0
A-Boomstick <ELLIOTT.SHAW22@bathspa.ac.uk>
2026-02-10 07:47:43 +0000

    Static web server + dynamic route
    All set up now. Just use a terminal console within VSC to run "node ." and go to "http://localhost:3000/data" and it'll do the getData function. Html file is for testing purposes

--- Project-Files/index.js
+++ Project-Files/index.js
@@ -1,1 +1,18 @@
-console.log("This worked dummy")
+const express = require("express");
+
+const app = express();
+
+app.use(express.urlencoded({extended: false}));
+
+app.listen(3000, () => console.log("Listening on http://localhost:3000"));
+
+app.use(express.static("./public"));
+
+// localhost:3000/test
+
+function getData(request, response) {
+    console.log("Test activated!");
+    response.send(Math.random());
+}
+
+app.get("/data", getData);
