51a5fe7529f086b394982a0220f9b2f68e56ec92
A-Boomstick <ELLIOTT.SHAW22@bathspa.ac.uk>
2026-03-27 10:45:39 +0000

    SQL to HTML
    SQL to Server to Client, now displays database stuff onto the client's page

--- Project-Files/public/main.js
+++ Project-Files/public/main.js
@@ -1,6 +1,8 @@
-const pushDatabase = document.getElementById("Input-Button");
+//const pushDatabase = document.getElementById("Input-Button");
+const pullDatabase = document.getElementById("DB-Button");
 
-async function pushClicked(event) {
+
+/*async function pushClicked(event) {
     const nameParam = document.getElementById("Name-Input").value;
     const ageParam = document.getElementById("Age-Input").value;
     const miscParam = document.getElementById("Misc-Input").value;
@@ -19,6 +21,70 @@ async function pushClicked(event) {
         }
     })
 
+} */
+
+async function pullClicked(event) {
+    //document.getElementById("DB-Button").textContent="It_Worked";
+    const response = await fetch("/db");
+    const responseData = await response.json();
+
+    let idList = document.getElementById("ID");
+    let nameList = document.getElementById("Username");
+    let passList = document.getElementById("Password");
+    let accessList = document.getElementById("AccessLevel");
+    let departmentList = document.getElementById("Department");
+
+    while (idList.hasChildNodes()){
+        idList.removeChild(idList.firstChild);
+    }
+
+    while (nameList.hasChildNodes()){
+        nameList.removeChild(nameList.firstChild);
+    }
+
+    while (passList.hasChildNodes()){
+        passList.removeChild(passList.firstChild);
+    }
+
+    while (accessList.hasChildNodes()){
+        accessList.removeChild(accessList.firstChild);
+    }
+
+    while (departmentList.hasChildNodes()){
+        departmentList.removeChild(departmentList.firstChild);
+    }
+
+    for (i = 0; i < responseData.length; ++i) {
+        let li = document.createElement('li');
+        li.appendChild(document.createTextNode(responseData[i].StaffID));
+        idList.appendChild(li)
+    } 
+
+    for (i = 0; i < responseData.length; ++i) {
+        let li = document.createElement('li');
+        li.appendChild(document.createTextNode(responseData[i].Username));
+        nameList.appendChild(li)
+    } 
+
+    for (i = 0; i < responseData.length; ++i) {
+        let li = document.createElement('li');
+        li.appendChild(document.createTextNode(responseData[i].Password));
+        passList.appendChild(li)
+    } 
+
+    for (i = 0; i < responseData.length; ++i) {
+        let li = document.createElement('li');
+        li.appendChild(document.createTextNode(responseData[i].AccessLevel));
+        accessList.appendChild(li)
+    } 
+
+    for (i = 0; i < responseData.length; ++i) {
+        let li = document.createElement('li');
+        li.appendChild(document.createTextNode(responseData[i].Department));
+        departmentList.appendChild(li)
+    } 
+
 }
 
-pushDatabase.addEventListener('click', pushClicked)
+//pushDatabase.addEventListener('click', pushClicked)
+pullDatabase.addEventListener('click', pullClicked)
